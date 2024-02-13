0c2195e8f125eef24dacffe87adb42c7f2d6e5ee
xiaoran-MENG <meng.xiaoran@outlook.com>
2024-02-12 20:47:33 -0600

    1

---
+++ src/q1/TwoSum.java
@@ -0,0 +1,17 @@
+package q1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // S(n) = O(n)
+        for (int i = 0; i < nums.length; i++) { // T(n) = O(n)
+            int diff = target - nums[i];
+            if (map.containsKey(diff))
+                return new int[] { map.get(diff), i };
+            map.put(nums[i], i);
+        }
+        throw new RuntimeException();
+    }
+}
